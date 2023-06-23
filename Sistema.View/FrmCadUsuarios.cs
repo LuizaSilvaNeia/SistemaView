039cc3ac971cd475644a6852a1d4d64961e1ad4d
LUIZA SILVA NEIA <luiza.sneia@senacsp.edu.br>
2023-06-23 17:07:52 -0300

    Usuarios

---
+++ Sistema.View/FrmCadUsuarios.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema.View
+{
+    public partial class FrmCadUsuarios : Form
+    {
+        public FrmCadUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            opc = "Novo";
+            HabilitarCampos();
+            LimparCampos();
+        }
+
+        private string opc = "";
+
+        private void InicioarOpc()
+        {
+            switch (opc)
+            {
+                case "Novo":
+                    HabilitarCampos();
+                    LimparCampos();
+                    break;
+                case "Salvar":
+                    break;
+                case "Excluir":
+                    break;
+            }
+        }
+        //criar as funções abaixo
+        private void HabilitarCampos()
+        {
+            txtNome.Enabled = true;
+            txtUsuario.Enabled = true;
+            txtSenha.Enabled = true;
+        }
+
+        private void LimparCampos()
+        {
+            txtNome.Text = "";
+            txtUsuario.Text = "";
+            txtSenha.Text = "";
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            opc = "Salvar";
+            InicioarOpc();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            opc = "Excluir";
+            InicioarOpc();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            opc = "Editar";
+            InicioarOpc();
+        }
+    }
+}
